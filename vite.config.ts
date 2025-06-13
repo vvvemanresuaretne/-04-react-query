3b4ec4b7fc639c6799a1721490f1454d1371f280
vvvemanresuaretne <vvvasilkovskyi@gmail.com>
2025-06-13 08:40:40 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'), // удобный импорт
-    },
-  },
-  server: {
-    open: true,
-    port: 3000,
-    strictPort: true,
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-  },
-  // ✅ Это важно для маршрутов (например, /movies) в SPA
-  base: '/',
-});
+})
